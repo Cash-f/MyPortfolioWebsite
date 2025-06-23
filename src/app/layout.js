3ae7136fab4cf9721b32ece8ac84b8453bdba601
Cash-f <charlieashfarmer09@gmail.com>
2025-06-23 20:06:34 +0100

    Add @vercel/speed-insights package and integrate SpeedInsights component in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Providers } from './providers'; 
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata = {
@@ -27,7 +28,7 @@ export default function RootLayout({ children }) {
   return (
     
     <html lang="en">
-      
+      <SpeedInsights/>
       <Providers>
         {children}
       </Providers>
